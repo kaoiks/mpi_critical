4b19fb502c4000f382f57575cc4eb4395924fec0
kaoiks354 <kox354@gmail.com>
2023-06-05 20:36:18 +0200

    Initial

---
+++ Makefile
@@ -0,0 +1,17 @@
+SOURCES=$(wildcard *.c)
+HEADERS=$(SOURCES:.c=.h)
+#FLAGS=-DDEBUG -g
+FLAGS=-g
+
+all: main
+
+main: $(SOURCES) $(HEADERS)
+	mpicc $(SOURCES) $(FLAGS) -o main
+
+clear: clean
+
+clean:
+	rm main a.out
+
+run: main
+	mpirun -oversubscribe -np 8 ./main 8 3
---
+++ watek_glowny.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include "watek_glowny.h"
+
+void mainLoop()
+{
+    srandom(time(NULL) + rank);
+    int tag;
+    int perc;
+
+    while (stan != InFinish) {
+	switch (stan) {
+	    case InRun: 
+			perc = random()%100;
+			if ( perc < 25 ) {
+				debug("Perc: %d", perc);
+				println("Ubiegam się o sekcję krytyczną")
+				debug("Zmieniam stan na wysyłanie");
+				packet_t *pkt = malloc(sizeof(packet_t));
+				pkt->data = perc;
+				ackCount = 0;
+
+				queue[rank] = timestamp;
+				
+				for (int i=0;i<=size-1;i++)
+				if (i!=rank)
+					sendPacket( pkt, i, REQUEST, timestamp);
+				++timestamp;	
+				changeState( InWant );
+				free(pkt);
+			}
+			debug("Skończyłem myśleć");
+		break;
+	    case InWant:{
+			// println("Czekam na wejście do sekcji krytycznej")
+			int beforeMeInLine = 0;
+			for(int i = 0; i < size; ++i){
+				if(queue[i] < queue[rank]){
+					++beforeMeInLine;
+				}
+			}
+
+			if ( ackCount == size - 1 && beforeMeInLine < B) 
+				changeState(InSection);
+			break;
+		}
+	    case InSection:
+		// tutaj zapewne jakiś muteks albo zmienna warunkowa
+		println("Jestem w sekcji krytycznej")
+		    sleep(5);
+		//if ( perc < 25 ) {
+		    debug("Perc: %d", perc);
+		    println("Wychodzę z sekcji krytyczneh")
+		    debug("Zmieniam stan na wysyłanie");
+		    packet_t *pkt = malloc(sizeof(packet_t));
+		    pkt->data = perc;
+		    for (int i=0;i<=size-1;i++)
+			if (i!=rank)
+			    sendPacket( pkt, (rank+1)%size, RELEASE, timestamp);
+			++timestamp;
+			queue[rank] = INT_MAX;
+			
+		    changeState( InRun );
+		    free(pkt);
+		//}
+		break;
+	    default: 
+		break;
+            }
+        sleep(SEC_IN_STATE);
+    }
+}
